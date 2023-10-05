5144a563050a4952caf34aa8679d74c929a25622
thiagosouza10 <thiagodesouza22@gmail.com>
2023-10-05 17:51:04 -0300

    [REPORT] Implementação

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -6,14 +6,15 @@ import Utilidades from "../support/utilidades"
 
 describe('Cadastro de usuários', () => {
 
+    beforeEach(() => {
+        Home.acessarHomeBugerEats(`${Cypress.env('urlBase')}`)
+        Home.acessarCadastro()
+    })
+
     it('Deve efetuar cadastro inserindo CNH com método de entrega Moto', () => {
-      
+
         const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
         const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
-
-        Home.acessarHomeBugerEats("https://buger-eats.vercel.app/")
-
-        Home.acessarCadastro()
 
         Deliver.preencherDadosDoUsuario({
             nomeCompleto: dadosUsuario.nomeCompleto,
@@ -27,8 +28,62 @@ describe('Cadastro de usuários', () => {
             numero: dadosEndereco.numero,
             complemento: dadosEndereco.complemento
         })
-        
+
         Deliver.selecionarMetodoEntrega('Moto')
+
+        Deliver.efetuarUploadCnh({
+            caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
+            nomeArquivo: 'cnh.jpg'
+        })
+
+        Deliver.validarCadastroSucesso()
+    })
+    it('Deve efetuar cadastro inserindo CNH com método de entrega Bicicleta', () => {
+
+        const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
+        const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
+
+        Deliver.preencherDadosDoUsuario({
+            nomeCompleto: dadosUsuario.nomeCompleto,
+            cpf: dadosUsuario.cpf,
+            email: dadosUsuario.email,
+            telefone: dadosUsuario.telefone
+        })
+
+        Deliver.preencherEnderecoUsuario({
+            cep: dadosEndereco.cep,
+            numero: dadosEndereco.numero,
+            complemento: dadosEndereco.complemento
+        })
+
+        Deliver.selecionarMetodoEntrega('Bicicleta')
+
+        Deliver.efetuarUploadCnh({
+            caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
+            nomeArquivo: 'cnh.jpg'
+        })
+
+        Deliver.validarCadastroSucesso()
+    })
+    it('Deve efetuar cadastro inserindo CNH com método de entrega Van/Carro', () => {
+
+        const dadosUsuario = Utilidades.gerarDadosFakerUsuario()
+        const dadosEndereco = Utilidades.gerarDadosFakerEndereco()
+
+        Deliver.preencherDadosDoUsuario({
+            nomeCompleto: dadosUsuario.nomeCompleto,
+            cpf: dadosUsuario.cpf,
+            email: dadosUsuario.email,
+            telefone: dadosUsuario.telefone
+        })
+
+        Deliver.preencherEnderecoUsuario({
+            cep: dadosEndereco.cep,
+            numero: dadosEndereco.numero,
+            complemento: dadosEndereco.complemento
+        })
+
+        Deliver.selecionarMetodoEntrega('Van/Carro')
 
         Deliver.efetuarUploadCnh({
             caminhoArquivo: 'cypress/support/imagem/cnh.jpg',
